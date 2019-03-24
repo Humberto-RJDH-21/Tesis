ac88cbd6db67a705d7e155e57707732e433e7acd
Humberto Ramírez Juárez <humberto.rjdh@gmail.com>
2019-03-24 13:18:05 -0600

    Initial Commit

---
+++ Template_Tesis_LaTeX/comandos.bat
@@ -0,0 +1,4 @@
+makeindex -s tesis.ist -t tesis.glg -o tesis.gls tesis.glo
+makeindex -s tesis.ist -t tesis.alg -o tesis.acn tesis.acr
+makeindex -s tesis.ist -t tesis.nlg -o tesis.ntn tesis.not
+pause
